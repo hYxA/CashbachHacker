6c18570d897c5692b0a6988cb12ec1c2f2d9fc79
Игорь <zybu@live.ru>
2021-09-09 19:03:21 +0300

    issue at here

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,26 +1,24 @@
 package ru.netology.service;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
-    int intForMessage;
 
     @Test
     public void NeedMoreBuy100() {
         int amount = 900;
         int expected = 100;
-        intForMessage = expected;
+//        intForMessage = expected;
 
         int actual = service.remain(amount);
 
         assertEquals(expected, actual);
 
-//        System.out.println("For get bonuses you need buy for " + expected + " rub");
+        System.out.println("For get bonuses you need buy for " + expected + " rub");
 
     }
 
@@ -28,17 +26,36 @@ class CashbackHackServiceTest {
     public void NeedMoreBuy10() {
         int amount = 990;
         int expected = 10;
-        intForMessage = expected;
+//        intForMessage = expected;
         int actual = service.remain(amount);
 
         assertEquals(expected, actual);
 
-//        System.out.println("For get bonuses you need buy for " + expected + " rub");
+        System.out.println("For get bonuses you need buy for " + expected + " rub");
+    }
+    @Test
+    public void NeedMoreBuy1000() {
+        int amount = 1000;
+        int expected = 0;
+//        intForMessage = expected;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void NoNeedMoreBuy2000() {
+        int amount = 2000;
+        int expected = 0;
+//        intForMessage = expected;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
     }
 
     @AfterEach
-    private void Message(int intForMessage) {
-        System.out.println("For get bonuses you need buy for " + intForMessage + " rub");
+    public void Message(int expected) {
+        System.out.println("For get bonuses you need buy for " + expected + " rub");
     }
 
 }
