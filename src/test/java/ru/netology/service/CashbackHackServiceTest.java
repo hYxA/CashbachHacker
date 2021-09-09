88a559c366d92c394bf65b3c812c3062123e1056
Игорь <zybu@live.ru>
2021-09-09 19:45:03 +0300

    Test passed and failed in JUnit4 and 5 in 1 file

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,65 +1,89 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
-//        int intForMessage;
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void NeedMoreBuy100() {
         int amount = 900;
         int expected = 100;
-//        intForMessage = expected;
 
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
 
         System.out.println("For get bonuses you need buy for " + expected + " rub");
 
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void NeedMoreBuy10() {
         int amount = 990;
         int expected = 10;
-//        intForMessage = expected;
         int actual = service.remain(amount);
 
-        assertEquals(expected, actual);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
 
         System.out.println("For get bonuses you need buy for " + expected + " rub");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void NoNeedMoreBuy1000() {
         int amount = 1000;
         int expected = 0;
-//        intForMessage = expected;
         int actual = service.remain(amount);
 
-        assertEquals(expected, actual);
-
-//        System.out.println("For get bonuses you need buy for " + expected + " rub");
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void NoNeedMoreBuy2000() {
         int amount = 2000;
         int expected = 0;
-//        intForMessage = expected;
+        int actual = service.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void NeedMoreBuy100JUnit4() {
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+
+        System.out.println("For get bonuses you need buy for " + expected + " rub");
+
+    }
+
+    @org.junit.Test
+    public void NeedMoreBuy10JUnit4() {
+        int amount = 990;
+        int expected = 10;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+
+        System.out.println("For get bonuses you need buy for " + expected + " rub");
+    }
+
+    @org.junit.Test
+    public void NoNeedMoreBuy1000JUnit4() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void NoNeedMoreBuy2000JUnit4() {
+        int amount = 2000;
+        int expected = 0;
         int actual = service.remain(amount);
 
         assertEquals(expected, actual);
-
-//        System.out.println("For get bonuses you need buy for " + expected + " rub");
     }
-
-//    @AfterEach
-//    private void Message(int intForMessage) {
-//        System.out.println("For get bonuses you need buy for " + intForMessage + " rub");
-//    }
-
 }
