bad034bd60bbb263b4709e65034f6d1d5766ade3
Игорь <zybu@live.ru>
2021-09-11 13:57:39 +0300

    issue at here

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,42 +1,36 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
+    int expected;
 
     @Test
     public void NeedMoreBuy100() {
         int amount = 900;
-        int expected = 100;
-//        intForMessage = expected;
-
+        expected = 100;
         int actual = service.remain(amount);
 
         assertEquals(expected, actual);
-
-        System.out.println("For get bonuses you need buy for " + expected + " rub");
-
     }
 
     @Test
     public void NeedMoreBuy10() {
         int amount = 990;
-        int expected = 10;
-//        intForMessage = expected;
+        expected = 10;
         int actual = service.remain(amount);
 
         assertEquals(expected, actual);
-
-        System.out.println("For get bonuses you need buy for " + expected + " rub");
     }
+
     @Test
     public void NeedMoreBuy1000() {
         int amount = 1000;
-        int expected = 0;
-//        intForMessage = expected;
+        expected = 0;
         int actual = service.remain(amount);
 
         assertEquals(expected, actual);
@@ -45,16 +39,15 @@ class CashbackHackServiceTest {
     @Test
     public void NoNeedMoreBuy2000() {
         int amount = 2000;
-        int expected = 0;
-//        intForMessage = expected;
+        expected = 0;
         int actual = service.remain(amount);
 
         assertEquals(expected, actual);
     }
 
-//    @AfterEach
-//    public void Message(int expected) {
-//        System.out.println("For get bonuses you need buy for " + expected + " rub");
-//    }
+    @AfterEach
+    public void Message(int expected) {
+        System.out.println("For get bonuses you need buy for " + expected + " rub");
+    }
 
 }
